a9f5c136f7765dae705d2659ca86d5a804a2f84c
Gyan Bano <gyanamurtiadhikarabano@gmail.com>
2023-10-30 00:36:06 +0700

    ajax nya belum bisa yang di admin

--- bookoo/urls.py
+++ bookoo/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('appmain.urls')),  # Tambahkan baris ini untuk mengimpor URL dari aplikasi 'appmain'
+    path('modulreport/', include('modulreport.urls'))
 ]
-#testes
